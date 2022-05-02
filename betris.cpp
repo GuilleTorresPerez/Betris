942ab1e16743a37af3b7e4b526dde25d467ef6c9
Guillermo Torres Pérez <guilletorresperez@gmail.com>
2022-05-02 13:48:11 +0200

    He añadido cositas a la función mostrarTablero
    Sé que no está bien del todo porque no utilizo el parámetro vEntrada pero no sé como aplicarlo

--- betris.cpp
+++ betris.cpp
@@ -11,7 +11,7 @@ void inicializarTablero(tpTablero &tablero) {
     }
 }
 
-//Esta la he creado yo para hacerlo más legible
+//Escribe en pantalla el color indicado
 void mostrarCasilla(const int codigoDeColor) {
     cout << "\033[" << codigoDeColor << "m" << " " << "\033[0m";
 }
@@ -19,7 +19,26 @@ void mostrarCasilla(const int codigoDeColor) {
 void mostrarTablero(const tpTablero & tablero, const int vEntrada[MAXENTRADA]) {
     for(int i = 0; tablero.nfils; i++) {
         for(int j = 0; tablero.ncols; j++) {
-            
+            switch(tablero.matriz[i][j]) {
+                case -1:
+                    mostrarCasilla(NEGRO);
+                    break;
+                case 0:
+                    mostrarCasilla(AZUL);
+                    break;
+                case 1:
+                    mostrarCasilla(VERDE);
+                    break;
+                case 2:
+                    mostrarCasilla(ROJO);
+                    break;
+                case 3:
+                    mostrarCasilla(CIAN);
+                    break;
+                case 4:
+                    mostrarCasilla(AMARILLO);
+                    break;
+            }
         }
     }
 }
